f785b1399a9dceebecf3feb42ef8ad08ccedf80a
kakedari <87064619+kakedari@users.noreply.github.com>
2021-12-15 14:43:43 +0530

    Create sample.groovy

---
+++ vars/sample.groovy
@@ -0,0 +1,18 @@
+int courseCount = 4;
+Boolean isProgrammer = true;
+String[] singers = ["Bob","Tom","Jeff","Roy"]
+
+if (isProgrammer){
+  println "He is a Programmer
+}
+else {
+  println "He is not a programmer"
+}
+
+for (int i=0; i<courseCount; i++){
+  println "Chirs made course" + (i+1) + "!!!"
+}
+
+for (String singer : singers){
+  println singer
+}
