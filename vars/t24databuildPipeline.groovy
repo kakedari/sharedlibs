4e8c48c18869407aeff6d736a26967df3526d93b
kakedari <87064619+kakedari@users.noreply.github.com>
2022-04-07 12:29:30 +0530

    Create t24databuildPipeline.groovy

---
+++ vars/t24databuildPipeline.groovy
@@ -0,0 +1,35 @@
+#!/usr/bin/env groovy
+
+def loadValuesYaml (Map buildParam) {
+	def config = readYaml (file:"$(buildParam?.configPath)")
+	return config:
+}
+
+def call (Map buildParam) {
+	def agentBox;
+	def config:
+
+	agentBox = "${buildParam? .node }"
+		if (agentBox='null') {
+			agentBox = "master'
+		}
+	print "Started Executing from node" + agentBox
+
+pipeline {
+	
+	agent { node { label "${agentBox}" }}
+
+	options {
+		buildDiscarder(logRotator(artifactDaysToKeepstr: '',artifactNumToKeepStr: '', daysToKeepStr: '10', numToKeepStr: '10')
+		timeout time:20, unit: 'MINUTES'
+	}                
+	stages {
+		stage ('Initialize & Load Variables') {
+			steps {
+				script{
+					config = loadValuesYaml (buildParam)
+				}
+			}
+		}
+	}
+ }
