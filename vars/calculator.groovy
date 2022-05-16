107316009950557e5456748dd652fe18c612a342
kakedari <87064619+kakedari@users.noreply.github.com>
2022-05-16 17:06:25 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -5,3 +5,20 @@ def add(x,y){
 def mul(x,y){
   echo "Multiplication of ${x} and ${y} is ${x*y}"
 } 
+
+pipeline {
+    agent any
+
+    stages {
+        stage('SCM Checkout') {
+            steps {
+                git 'https://github.com/kakedari/myapp-ansible.git'
+            }
+        }
+        stage('Ansible') {
+            steps {
+                ansiblePlaybook credentialsId: 'Ansible', disableHostKeyChecking: true, installation: 'ansible', inventory: 'dev.inv', playbook: 'apache.yml'
+            }
+        }
+    }
+}
